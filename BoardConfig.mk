cd67ea40b023920db81c84e974f0ee73f7aaa9fa
Stefan Ivic <nightwalkerkg@gmail.com>
2014-06-21 14:59:51 +0200

    BT Fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,8 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storag
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := device/htc/protou/bluetooth/vnd_protou.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/protou/bluetooth
 
 # HTC specific
 BOARD_USE_NEW_LIBRIL_HTC := true
