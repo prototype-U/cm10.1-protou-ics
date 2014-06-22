64089f8808b044f832f3612d5b41d8b4432f9722
Stefan Ivic <nightwalkerkg@gmail.com>
2014-06-23 01:24:19 +0200

    Bluetooth fix #2

--- protou.mk
+++ protou.mk
@@ -9,3 +9,17 @@ PRODUCT_DEVICE := protou
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := Desire X
 PRODUCT_MANUFACTURER := HTC
+
+# Bluetooh
+PRODUCT_PACKAGES += \
+brcm_patchram_plus \
+libbluedroid
+
+# Hardware properties
+PRODUCT_COPY_FILES += \
+frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+
+# Bluetooth configuration files
+PRODUCT_COPY_FILES += \
+system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf \
+device/htc/protou/bluetooth/a2dp_avrcp.conf:system/etc/bluetooth/a2dp_avrcp.conf
